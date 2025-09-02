39da5702a82364c82351029652aa97ca2ab3df8d
Ivan Eidelstein <ivan.eidelstein@gmail.com>
2025-09-02 20:59:32 -0300

    feat: Review percentage

--- src/review/index.tsx
+++ src/review/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Flashcard } from "../types";
 import { useFlashcards } from "../flashcard/context/FlashcardContext";
 import { shuffle } from "lodash";
@@ -12,12 +12,20 @@ const ReviewFlashcards = () => {
   const [flashcardsToReview, setFlashcardsToReview] = useState<Flashcard[]>([]);
   const [active, setActive] = useState(0);
 
+  const reviewPercentage = useMemo(
+    () => (active * 100) / (flashcards.length - 1),
+    [active, flashcards]
+  );
+
   useEffect(() => {
     setFlashcardsToReview(shuffle(flashcards));
   }, [flashcards]);
 
   return (
     <div className="review">
+      <div className="review-completion-percentage">
+        <div className="bar" style={{ width: `${reviewPercentage}%` }} />
+      </div>
       {flashcardsToReview.map((flashcard, index) => {
         if (index !== active) return null;
 
