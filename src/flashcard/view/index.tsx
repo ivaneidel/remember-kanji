6c1226e937981a6648173b53e6ec930b1f063d8f
Ivan Eidelstein <ivan.eidelstein@gmail.com>
2025-09-07 20:56:58 -0300

    feat: Highlight primitive in helps

--- src/flashcard/view/index.tsx
+++ src/flashcard/view/index.tsx
@@ -31,6 +31,22 @@ const FlashcardView = ({
     return flashcardsById[params.flashcardId];
   }, [paramFlashcard, params.flashcardId, flashcardsById]);
 
+  const help = useMemo(() => {
+    if (!flashcard?.help) return null;
+
+    const chunks = flashcard.help.trim().split(" ");
+
+    return chunks.map((s, index) => {
+      const isLowerCase = s.toLowerCase() === s;
+      const isFirstWord = index === 0;
+      const isAfterDot = chunks[index - 1] && chunks[index - 1].endsWith(".");
+
+      if (isLowerCase || isFirstWord || isAfterDot) return <span>{s}</span>;
+
+      return <span className="primitive">{s}</span>;
+    });
+  }, [flashcard]);
+
   if (!flashcard) return null;
 
   return (
@@ -47,7 +63,7 @@ const FlashcardView = ({
       </div>
       <div className="back">
         <span className="key-word">{flashcard.keyWord.toUpperCase()}</span>
-        {flashcard.help && <span className="help">{flashcard.help}</span>}
+        {help && <div className="help">{...help}</div>}
         {extraContentBack}
       </div>
     </div>
